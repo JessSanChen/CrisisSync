881532e4fdeda9548da98761ecdff245fe962d04
Ronak Malik <ronakmalik4242@gmail.com>
2025-02-15 14:46:22 -0500

    python sucks

--- backend/main.py
+++ backend/main.py
@@ -29,10 +29,6 @@ async def read_tweets():
     return data
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "query": q}
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
