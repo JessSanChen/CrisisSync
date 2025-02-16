07d20b90c04e478f652e8af6fc7300884abcaf27
Jessica Chen <jessicachen@dhcp-10-250-162-89.harvard.edu>
2025-02-15 19:23:38 -0500

    m

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -173,11 +173,10 @@ export default function LandingPage() {
           </div>
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+              CrisisSync
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat.
+            Coordinating relief, saving lives.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
